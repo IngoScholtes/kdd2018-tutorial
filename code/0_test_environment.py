bfd0abadb5e44e47f4718db3b2cbdda89c63467c
Ingo Scholtes <ischoltes@ethz.ch>
2018-08-16 11:40:22 +0200

    adding path check to test environment

--- code/0_test_environment.py
+++ code/0_test_environment.py
@@ -1,7 +1,14 @@
-#%%
+"""
+This is a test file, that you can use to validate 
+"""
+
+#%% validate that pathpy was installed correct
 import pathpy as pp
 paths = pp.Paths()
 paths.add_path('a,b,c')
-
-#%%
 print(paths)
+
+#%% validate that kernel was started in correct root directory
+t = pp.TemporalNetwork.read_file('data/temporal_clusters.tedges')
+print(t)
+
