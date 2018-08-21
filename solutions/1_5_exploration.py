b6c4f64d743da7da4b126e22596a6c169a38edb9
Ingo Scholtes <ischoltes@ethz.ch>
2018-08-21 17:26:56 +0100

    updated 1.5 in solutions

--- solutions/1_5_exploration.py
+++ solutions/1_5_exploration.py
@@ -6,7 +6,7 @@ def md(str):
 
 #%%
 md("""
-# 1.4 Exploration: Higher-order analysis of real-world pathway data
+# 1.5 Exploration: Higher-order analysis of real-world pathway data
 
 **Ingo Scholtes**  
 Data Analytics Group  
@@ -16,16 +16,13 @@ University of Zurich
 
 **August 22 2018**
 
-In the last (open-ended) exploration of this first tutorial session, you have the chance to use higher-order network analytics to study three real data sets for yourself:
+In the last (open-ended) exploration of this first tutorial session, you have the chance to use higher-order network analytics to study real data sets for yourself.
 
-- Passenger itineraries constructed from London Tube Oystercard origin-destination data
-- Flight itineraries of US airline passengers
-- Wikipedia clickstream data
-
-Details on the origin of these data can be found [here](https://github.com/IngoScholtes/kdd2018-tutorial/tree/master/data). Below, we include the code to load these data sets in `pathpy`. Using these data and the methods introduced in the first tutorial session, we suggest to address the following problems (in ascending order of difficulty):
+Details on the available data sets can be found [here](https://github.com/IngoScholtes/kdd2018-tutorial/tree/master/data). Using these methods introduced in the first tutorial session, we suggest you to address the following problems (in ascending order of difficulty):
 
 - Repeat the analysis of higher-order centralities in the toy example from 1.3 with the closeness centrality of nodes. What do you observe?
 - Test the prediction performance of higher-order models for the London Tube and/or the Wikipedia clickstream data set. Does the prediction performance saturate at k=2 as it does for the US Flight data?
+- Study the difference between higher- and first-order centralities in the dynamic social networks contained in the SQLite database file.
 - Use the higher-order framework to identify those paths of length k that show "anomalous statistics" (compared to a memoryless null model). Which are these paths and how can we interpret the result?
 
 Please consider the data sets as well as the questions above as mere **suggestions for your exploration of higher-order network analytics**. You are welcome to study other data sets or questions instead. Please reach out to me if you encounter any problems or questions (also after KDD). You can reach me at `scholtes@ifi.uzh.ch`.
